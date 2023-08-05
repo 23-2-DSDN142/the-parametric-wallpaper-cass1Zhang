8643b7905ffeb7c5615939f7bb67af472b06a480
cass1Zhang <140350029+cass1Zhang@users.noreply.github.com>
2023-08-05 19:36:42 +1200

    Update my_wallpaper.js

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -18,6 +18,8 @@ function wallpaper_background() {
 
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
   //雪人
+
+  //帽子
   fill(247, 147, 184);
   noStroke();
   beginShape();
@@ -25,38 +27,66 @@ function my_symbol() { // do not rename this function. Treat this similarly to a
   vertex(157.5, 142.75);
   vertex(169.875, 136.375);
   endShape();
+
+  //帽子上的球
+  fill(247, 147, 184);
+  ellipse(157, 123, 10,10)
+
+  //高光
+
+  fill(255);
+  ellipse(160, 122, 1, 2)
+
+
   
-  fill(255, 255, 255);
+
+
+
+
+  //下半身
+  fill(255);
   noStroke();
   ellipse(166.875, 171.25, 37.5, 37.5);
+
+   //纽扣
+   fill(247, 147, 184);
+   noStroke();
+   ellipse(169, 160, 4,4 );
+   ellipse(169, 170, 4,4);
+   ellipse(169, 180, 4,4);
   
+  //领结
   fill(247, 147, 184);
   noStroke();
   beginShape();
-  vertex(163.875, 151.75);
-  vertex(165, 156.25);
-  vertex(168.75, 153.375);
+  vertex(160, 152);
+  vertex(167, 157);
+  vertex(169, 154);
   endShape();
   beginShape();
-  vertex(168.75, 153.375);
-  vertex(171.75, 155.125);
-  vertex(172.5, 150.875);
+  vertex(169, 154);
+  vertex(172, 156);
+  vertex(173, 151);
   endShape();
   
+  //头
   fill(255, 255, 255);
   noStroke();
   ellipse(166.125, 145, 18.75, 18.75);
   
+  //眼睛
   fill(0, 0, 0);
   noStroke();
   ellipse(163.5, 143.875, 1.5, 1.5);
   ellipse(169.875, 142.375, 1.5, 1.5);
   
+  //腮红
   fill(255, 185, 191);
   noStroke();
   ellipse(163.875, 147.625, 4.875, 4.5);
   ellipse(172.125, 145, 3.75, 3.75);
   
+  //鼻子
   fill(255, 177, 98);
   noStroke();
   beginShape();
@@ -67,7 +97,7 @@ function my_symbol() { // do not rename this function. Treat this similarly to a
   
   ellipse(168, 146.125, 3, 3);
   
-  
+
   
     
   
@@ -78,38 +108,76 @@ function my_symbol() { // do not rename this function. Treat this similarly to a
   //企鹅
     noStroke();
     //腿
+    stroke(50)
     fill(255, 160, 45);
-    ellipse(22.6, 58, 11.11, 5.56);
-    ellipse(44.5, 58, 11.11, 5.56);
+    ellipse(25, 58, 11.11, 5.56);
+    ellipse(42, 58, 11.11, 5.56);
 
     
     //身体
-    fill(186, 185, 184);
-    ellipse(16, 43, 5, 20);//左手 
-    ellipse(51, 43, 5, 20);//右手
-    ellipse(33.5, 43, 40, 30);//肚皮
-    //头
+    fill(0);
+    ellipse(18, 42, 7, 20);//左手 
+    ellipse(49, 42, 7, 20);//右手
+    ellipse(33.5, 40, 37, 40);//肚皮
 
+    //肚子
+    noStroke()
+    fill(255);
+    ellipse(33,45,18,20)
+
+    //脖子
+    fill(0)
+    ellipse(33,27,32,15)
     
-    strokeWeight(33.33);
-    stroke(186, 185, 184);
-    line(33.33, 23.89, 33.33, 36.67);
+
+    //脑壳
+    fill(35)
+    ellipse(33, 23, 30,31.5)
+
+  
+   
     //眼睛，肚皮
     fill(255);
     noStroke();
-    ellipse(27, 23,9, 9);//左眼
-    ellipse(39, 23, 9, 9);//右眼
-    ellipse(33.33,26, 19, 10);//肚皮
+    ellipse(28, 23,9, 15);//左眼
+    ellipse(39, 23, 9, 15);//右眼
+
+     //脸
+     fill(255);
+     noStroke();
+     ellipse(33.33,27, 23, 10);
+
+     //腮红
+     fill(255, 160, 45);
+     ellipse(27, 26, 6, 6);
+     ellipse(39, 26, 6, 6);
+    
+   
+
+
+
     //眼球
     fill(0);
-    ellipse(27, 23, 2, 2);//左眼球
-    ellipse(39, 23, 2, 2);//右眼球
+    ellipse(28, 23, 2.8, 2.8);//左眼球
+    ellipse(38, 23, 2.8, 2.8);//右眼球
+
+     //高光
+     fill(255);
+     ellipse(28.9, 23,1,1);
+     ellipse(38.9, 23,1,1);
+
+     
 
     //嘴巴
     
     fill(255, 160, 45);
 
-    ellipse(33, 23, 7, 5);
+    ellipse(33, 23, 6, 4);
+
+
+    
+
+    
     
   
      //Cat
@@ -156,6 +224,7 @@ function my_symbol() { // do not rename this function. Treat this similarly to a
      stroke(86, 54, 33);
      fill(0);
      ellipse(103, 88, 40, 35);
+
      
      // 绘制眼睛
      fill(253, 249, 202);
@@ -164,6 +233,12 @@ function my_symbol() { // do not rename this function. Treat this similarly to a
      fill(0);
      ellipse(93.4, 88, 9, 11);
      ellipse(112.2, 88, 9, 11);
+
+     //高光
+     fill(255);
+     noStroke()
+     ellipse(97, 89, 4, 4)
+     ellipse(116, 89, 4, 4)
      
      // 绘制嘴
      fill(52, 119, 164);
@@ -205,4 +280,23 @@ function my_symbol() { // do not rename this function. Treat this similarly to a
      vertex(88.1, 114.4);
      bezierVertex(88.12, 114.4, 89, 118, 91, 118);
      endShape();
+
+
+//猫爪
+fill(255, 185, 191)
+noStroke()
+ellipse(26, 180, 10, 10)
+ellipse(34, 180, 10, 10)
+ellipse(30, 175, 10, 10)
+
+
+
+ellipse(20, 170, 5, 10)
+ellipse(26, 164, 5, 10)
+ellipse(33, 164, 5, 10)
+ellipse(40, 170, 5, 10)
+
+
+
+
 }     
