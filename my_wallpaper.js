1fc3f9578318d9840f9495456cc05a7ba62e3349
cass1Zhang <140350029+cass1Zhang@users.noreply.github.com>
2023-08-05 16:41:20 +1200

    Update my_wallpaper.js

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -2,7 +2,7 @@
 
 
 function setup_wallpaper(pWallpaper) {
-  pWallpaper.output_mode(DEVELOP_GLYPH);
+  pWallpaper.output_mode(GRID_WALLPAPER);
   pWallpaper.resolution(FIT_TO_SCREEN);
   pWallpaper.show_guide(true); //set this to false when you're ready to print
 
